aa72580bfa3ef616abcd1deedd872a12d01bde5e
frankcrest <frankcrestview@hotmail.com>
2018-10-09 22:24:45 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Frank Chen on 2018-10-09.
+//  Copyright © 2018 Frank Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
